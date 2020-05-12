c2fef2658cbe98479b150d649091be96b1427d8d
Joshua Armoney <armoneyj210@gmail.com>
2020-05-12 14:44:58 -0400

    product.js model

---
+++ models/product.js
@@ -0,0 +1,38 @@
+const mongoose = require("./connection.js");
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  img: String,
+  cat: String,
+});
+
+const productCollection = mongoose.model("Product", ProductSchema);
+
+const getProducts = () => {
+  return productCollection.find({});
+};
+
+const getProductsById = (id) => {
+  return productCollection.findById(id);
+};
+
+const createProduct = (productObject) => {
+  return productCollection.create(productObject);
+};
+
+const deleteProduct = (id) => {
+  return productCollection.deleteOne({ _id: id });
+};
+
+const updateProduct = (id, updatedProduct) => {
+  return productCollection.updateOne({ _id: id }, updatedProduct);
+};
+module.exports = {
+  getProducts,
+  getProductsById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+};
