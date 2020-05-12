d3c55d924af6b8c269623f004d7ee2938690753c
Joshua Armoney <armoneyj210@gmail.com>
2020-05-12 11:56:05 -0400

    init setup

---
+++ client/build/precache-manifest.6521f14dcd35e8e1db67497d2dcf80db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9103298e1f5219c990ae9975121064d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c701e84f9a681824d1f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "4c701e84f9a681824d1f",
+    "url": "/static/js/main.21aec0b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
