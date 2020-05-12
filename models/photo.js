0062c78ca6e74286b196c739e83cadbdbda68d31
Joshua Armoney <armoneyj210@gmail.com>
2020-05-12 14:52:14 -0400

    photo.js model

---
+++ models/photo.js
@@ -0,0 +1,35 @@
+const mongoose = require("./connection.js");
+
+const PhotoSchema = new mongoose.Schema({
+  name: String,
+  img: String,
+});
+
+const photoCollection = mongoose.model("Photo", PhotoSchema);
+
+const getPhotos = () => {
+  return photoCollection.find({});
+};
+
+const getPhotosById = (id) => {
+  return photoCollection.findById(id);
+};
+
+const createPhoto = (photoObject) => {
+  return photoCollection.create(photoObject);
+};
+P;
+const deletePhoto = (id) => {
+  return photoCollection.deleteOne({ _id: id });
+};
+
+const updatePhoto = (id, updatedPhoto) => {
+  return photoCollection.updateOne({ _id: id }, updatedPhoto);
+};
+module.exports = {
+  getPhotos,
+  getPhotosById,
+  createPhoto,
+  deletePhoto,
+  updatePhoto,
+};
