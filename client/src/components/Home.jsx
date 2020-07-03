a61ee8edb84a3ecd8b92997db3fafdb23661c735
Joshua Armoney <armoneyj210@gmail.com>
2020-07-02 22:46:02 -0400

    Home.js done/ need contact form and more info

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 import Footer from "./Footer";
 export default class Home extends Component {
   render() {
-    return <div className="placeholder"></div>;
+    return (
+      <div className="home-flex-container">
+        <div className="home-flex-item home-font-up">
+          <h1>Vintage Dirt Bike Restoration</h1>
+        </div>
+        ||
+        <div className="home-flex-item home-font-up">
+          <h1>Harley Builds</h1>
+        </div>
+        ||
+        <div className="home-flex-item">
+          <h5>Suspension</h5>
+        </div>
+        <div className="home-flex-btn-cont">
+          <a href="/photo">
+            <button className="home-flex-btn">Come on in!</button>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
